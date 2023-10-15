8d84fd5b6e560f3066008dae98dc20ab7626eee9
Onani-Master <phuthinhqq@gmail.com>
2023-10-15 10:37:28 +0700

    h

--- server.js
+++ server.js
@@ -1,72 +1,75 @@
 // server.js
-//console.log('May Node be with you')
-
 const express = require('express');
-const bodyParser= require('body-parser')
+const bodyParser = require('body-parser');
 const app = express();
 
-const MongoClient = require('mongodb').MongoClient
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
 
-const connectionString = 'mongodb+srv://onani_master:2003@cluster0.7sjmpmz.mongodb.net/'
+const connectionString = 'mongodb+srv://onani_master:2003@cluster0.7sjmpmz.mongodb.net/';
 
-// (0) CONNECT: server -> connect -> MongoDB Atlas 
 MongoClient.connect(connectionString, { useUnifiedTopology: true })
     .then(client => {
         console.log('Connected to Database')
         
-        // (1a) CREATE: client -> create -> database -> 'star-wars-quotes'
-        // -> create -> collection -> 'quotes'
         const db = client.db('star-wars-quotes')
         const quotesCollection = db.collection('quotes')
         
-        // To tell Express to EJS as the template engine
         app.set('view engine', 'ejs') 
-        
-        // Make sure you place body-parser before your CRUD handlers!
         app.use(bodyParser.urlencoded({ extended: true }))
-
-        // To make the 'public' folder accessible to the public
         app.use(express.static('public'))
-
-        // To teach the server to read JSON data 
         app.use(bodyParser.json())
 
-        // (2) READ: client -> browser -> url 
-        // -> server -> '/' -> collection -> 'quotes' -> find() 
-        // -> results -> index.ejs -> client
         app.get('/', (req, res) => {
             db.collection('quotes').find().toArray()
                 .then(results => {
-
-                    // results -> server -> console
-                    console.log(results)
-                    
-                    // results -> index.ejs -> client -> browser 
-                    // The file 'index.ejs' must be placed inside a 'views' folder BY DEFAULT
                     res.render('index.ejs', { quotes: results })
                 })
-                .catch(/* ... */)
+                .catch(error => console.error(error))
         })
 
-        // (1b) CREATE: client -> index.ejs -> data -> SUBMIT 
-        // -> post -> '/quotes' -> collection -> insert -> result
         app.post('/quotes', (req, res) => {
             quotesCollection.insertOne(req.body)
-            .then(result => {
-                
-                // results -> server -> console
-                console.log(result)
+                .then(result => {
+                    res.redirect('/')
+                })
+                .catch(error => console.error(error))
+        })
 
-                // -> redirect -> '/'
-                res.redirect('/')
-             })
-            .catch(error => console.error(error))
+        // Edit route
+        app.get('/quotes/:id/edit', (req, res) => {
+            const quoteId = req.params.id;
+            quotesCollection.findOne({ _id: new ObjectID(quoteId) })
+                .then(result => {
+                    res.render('edit.ejs', { quote: result })
+                })
+                .catch(error => console.error(error))
+        })
+
+        // Update route
+        app.post('/quotes/:id/update', (req, res) => {
+            const quoteId = req.params.id;
+            quotesCollection.updateOne(
+                { _id: new ObjectID(quoteId) },
+                { $set: { name: req.body.name, quote: req.body.quote } }
+            )
+                .then(result => {
+                    res.redirect('/')
+                })
+                .catch(error => console.error(error))
+        })
+
+        // Delete route
+        app.post('/quotes/:id/delete', (req, res) => {
+            const quoteId = req.params.id;
+            quotesCollection.deleteOne({ _id: new ObjectID(quoteId) })
+                .then(result => {
+                    res.redirect('/')
+                })
+                .catch(error => console.error(error))
         })
         
-        // server -> listen -> port -> 3000
         app.listen(3000, function() {
             console.log('listening on 3000')
         })
     })
-
-
